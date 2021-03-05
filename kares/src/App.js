44ce83e8bc4f9ef5b0a34e055e3a066403d740cc
Zach Shearer <shearertzach@gmail.com>
2021-03-05 01:01:41 -0600

    Navbar, Footer, Homepage
    Added various files to complete the homepage. Created the navbar and footer component to complete the homepage.

--- kares/src/App.js
+++ kares/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { HashRouter as Router, Route } from 'react-router-dom'
+import Homepage from './screens/Homepage/Homepage';
 
 function App() {
   return (
     <Router>
-      
+      <Route exact path='/'>
+        <Homepage />
+      </Route>
     </Router>
   );
 }
