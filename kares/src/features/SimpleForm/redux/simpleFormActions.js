6171dc3e8f8d9902e015883bc2a35733b3240b36
Zach Shearer <shearertzach@gmail.com>
2021-03-05 22:10:33 -0600

    Created simpleform with some issues to be looked at

--- kares/src/features/SimpleForm/redux/simpleFormActions.js
+++ kares/src/features/SimpleForm/redux/simpleFormActions.js
@@ -1,3 +1,6 @@
+import { db } from '../../../shared/services/firebaseConfig'
+
+
 export const openSimpleForm = () => ({
     type: 'OPEN_FORM',
     payload: true
@@ -7,3 +10,10 @@ export const closeSimpleForm = () => ({
     type: 'CLOSE_FORM',
     payload: false
 })
+
+export const simpleFormSubmit = (formData) => dispatch => {
+
+    const userCollection = db.collection('users')
+
+    userCollection.doc(formData.email).set(formData)
+}
